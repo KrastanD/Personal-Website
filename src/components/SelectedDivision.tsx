098cf8a3941340a746cf8c7d1e2bef38239be35d
Krastan Dimitrov <krastan.kdd@gmail.com>
2022-07-11 18:35:59 -0500

    Updating Divisions import

--- src/components/SelectedDivision.tsx
+++ src/components/SelectedDivision.tsx
@@ -1,4 +1,4 @@
-import { Divisions } from "../App";
+import { Divisions } from "../types";
 
 const SelectedDivision = ({
   hasTitle,
--- src/components/UnselectedDivision.tsx
+++ src/components/UnselectedDivision.tsx
@@ -1,4 +1,4 @@
-import { Divisions } from "../App";
+import { Divisions } from "../types";
 
 const UnselectedDivision = ({ division }: { division: Divisions }) => {
   return (
