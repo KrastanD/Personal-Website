6b222ab9eb43cf52d6a6dfeede5ceb49551aaffa
Krastan Dimitrov <krastan.kdd@gmail.com>
2022-10-24 10:23:04 -0500

    Update Education Page with card design

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,42 +1,37 @@
+import uiuc from "../assets/uiuc-siebel.jpg";
+import cod from "../assets/COD.jpeg";
+import wnhs from "../assets/WNHS.jpeg";
+import EducationCard from "./EducationCard";
+
 const Education = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 text-gray-300">
-      <p>
-        <strong>University of Illinois at Urbana-Champaign</strong>
-        <br />
-        2018-2020
-      </p>
-      <p>
-        My alma mater. Of the 61 credits hours I took, 52 were computer science
-        courses. I managed to get my degree in only two years with the help of
-        my college and AP credits. I enjoyed my time there thoroughly and
-        learned so much from the brilliant professors and fellow students. My
-        focus was on Big Data, because I took courses about databases,
-        artificial intelligence and machine learning. I took classes in many
-        other fields though, as I love how broad computer science is and how
-        much there is to learn.
-      </p>
-      <p>
-        <strong>College of DuPage</strong>
-        <br />
-        2016-2018
-      </p>
-      <p>
-        A community college in the suburbs of Chicago where I took the majority
-        of my Gen-Eds and engineering requirements. I had to keep a 4.0 GPA
-        prior to applying to UIUC while working 20 hours per week. I learned a
-        lot about time management and how to get the most out of myself.
-      </p>
-      <p>
-        <strong>Wheaton North High School</strong>
-        <br />
-        2012-2016
-      </p>
-      <p>
-        My high school where I took my first computer science course and my love
-        for programming began. I was also a four-year cross country and track
-        and field runner.
-      </p>
+    <div className="flex flex-col items-center">
+      <EducationCard
+        title="University of Illinois at Urbana-Champaign"
+        image={uiuc}
+        description="My alma mater. Of the 61 credits hours I took, 52 were computer science
+        courses. I earned my degree in only two years with the help of my
+        college and AP credits. I enjoyed my time there thoroughly and learned
+        so much from the brilliant professors and peers. My focus was on Big
+        Data, because I took courses about databases, artificial intelligence
+        and machine learning. I took classes in many other fields though, as I
+        love how broad computer science is and how much there is to learn."
+      />
+      <EducationCard
+        title="College of DuPage"
+        image={cod}
+        description="A community college in the suburbs of Chicago where I took the majority
+      of my Gen-Eds and engineering requirements. I had to keep a 4.0 GPA
+      prior to applying to UIUC while working 20 hours per week. I learned a
+      lot about time management and how to get the most out of myself."
+      />
+      <EducationCard
+        title="Wheaton North High School"
+        image={wnhs}
+        description="My high school where I took my first computer science course and my love
+      for programming began. I was also a four-year cross country and track
+      and field runner."
+      />
     </div>
   );
 };
---
+++ src/components/EducationCard.tsx
@@ -0,0 +1,25 @@
+const EducationCard = ({
+  title,
+  description,
+  image,
+}: {
+  title: string;
+  description: string;
+  image: string;
+}) => {
+  return (
+    <div className="flex flex-col lg:flex-row bg-gray-300 m-3 rounded text-black">
+      <div className="p-4">
+        <p className="text-center text-2xl font-bold mb-3">{title}</p>
+        <p>{description}</p>
+      </div>
+      <img
+        className="w-full lg:w-64 xl:w-80 rounded object-cover"
+        src={image}
+        alt="School image"
+      />
+    </div>
+  );
+};
+
+export default EducationCard;
