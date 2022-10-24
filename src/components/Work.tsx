7dd56ba2e9b3cae0908be3592bc92a51ba20e21b
Krastan Dimitrov <krastan.kdd@gmail.com>
2022-10-24 17:30:45 -0500

    Update Work page with cards

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,51 +1,52 @@
+import WorkCard, { WorkListItem } from "./WorkCard";
+
 const Work = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 text-gray-300">
-      <p>
-        <strong>Fubo Gaming</strong>
-        <br />
-        March 2021 - Current
-      </p>
-      <p>
-        <strong>Software Engineer</strong>
-        <br />
-        Fubo Gaming is the sportsbook subsidiary of fuboTV. I am continuing my
-        work from Vigtory and developing the mobile application.
-      </p>
-      <p>
-        <strong>Vigtory</strong>
-        <br />
-        September 2020 - March 2021
-      </p>
-      <p>
-        <strong>Mobile Software Engineer</strong>
-        <br />
-        Vigtory was a Chicago-based sportsbook where I worked on the Android and
-        iOS app. The app is built with React Native and TypeScript and uses a
-        GraphQL API. I enjoyed working in the fast-paced environment of a
-        start-up where I was being challenged on a daily basis. I was able to
-        work on a variety of projects including upgrading the app from React
-        Navigation 4 to 5, implementing a third-party API displaying articles
-        and refactoring components such as consolidating four tab components
-        into a single reusable one.
-      </p>
-
-      <p>
-        <strong>Ecolab</strong>
-        <br />
-        June 2019 - August 2019
-      </p>
-      <p>
-        <strong>Developer Intern</strong>
-        <br />
-        Ecolab was my first experience working in the corporate world. I learned
-        a lot about collaborating with a team on a software solution. Working
-        with a product manager, presenting and delivering the product was a
-        valuable experience. My biggest challenges were dealing with changing
-        requirements and importing the data we were given into a database. What
-        I enjoyed the most was learning and applying new techniques to our
-        project like MVC, authentication, and authorization.
-      </p>
+    <div className="flex flex-col text-gray-300">
+      <WorkCard
+        title="FuboTV"
+        years="March 2021 - November 2022"
+        position="Software Engineer"
+      >
+        <WorkListItem text="Architected and lead development of the FuboTV/Sportsbook API service integration with Sportsbook React Native mobile application to provide product with key market differentiator" />
+        <WorkListItem text="Designed and developed streamlined web User Landing Experience for Sportsbook using NextJS and TailwindCSS to increase user conversion and decrease CAC" />
+        <WorkListItem text="Migrated legacy React Native codebase to newer React Native framework while adding developer-friendly features like Flipper, Apple Silicon support, Hermes and fixing third-party dependency/resolution conflicts" />
+        <WorkListItem text="Implemented private library to modularize and decouple complex authentication flow logic from web and mobile clients while streamlining user experience to comply with strict regulatory constraints" />
+        <WorkListItem text="Added deep linking (Universal Links and App Links), deferred deep linking, and analytics to mobile app to improve authentication flow conversion and track attribution through Appsflyer" />
+        <WorkListItem text="Introduced unit/integration testing using Jest/React Native Test Library to private libraries and adopted Storybook for UI design and stakeholder validation of UI components" />
+        <WorkListItem text="Mentored developers in React Native development and tooling by pair programming to increase team velocity and raise code quality" />
+        <WorkListItem text="Containerized React Native application using Docker to improve build consistency and improve developer experience" />
+        <WorkListItem text="Lead Scrum rituals and demos in the agile process to provide key stakeholders with an overview of the teams work in an easily comprehensible way to showcase business value" />
+      </WorkCard>
+      <WorkCard
+        title="Vigtory"
+        years="September 2020 - March 2021"
+        position="Mobile Software Engineer"
+      >
+        <WorkListItem text="Integrated a tabbed feed of expert content from a third party provider using a REST API enabling users to view articles in app to increase app use duration and user retention" />
+        <WorkListItem text="Refactored multiple bet slip components using techniques like extracting duplicated code, breaking up large components into modular, maintainable ones, and simplifying conditional expressions" />
+        <WorkListItem text="Increased UI consistency by consolidating five different tab React components into one" />
+        <WorkListItem text="Overhauled and upgraded in-app navigation to React Navigation 5 for a more natural and pleasant user experience" />
+      </WorkCard>
+      <WorkCard
+        title="Ecolab"
+        years="June 2019 - August 2019"
+        position="Developer Intern"
+      >
+        <WorkListItem
+          text="Effectively worked in a team of 6 using Scrum to design, develop and deploy a dashboard in ASP.NET to display
+over-shipment of fixed contracts on three levels, saving an estimated $10 million loss of revenue in operating
+income"
+        />
+        <WorkListItem
+          text="Delivered value by increasing visibility of shipment data, reducing manual effort to track lost revenue, and
+predicting shipment ahead of time"
+        />
+        <WorkListItem
+          text="Acquired an understanding for the importance of teamwork with strong leadership at all levels of the corporate
+structure through meetings with the company’s CEO during a team outing to St. Paul, MN"
+        />
+      </WorkCard>
     </div>
   );
 };
