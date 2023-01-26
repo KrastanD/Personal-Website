2e3d6faa194c6fd34073c1e5104877a748f6a0a0
Krastan Dimitrov <krastan.kdd@gmail.com>
2023-01-25 23:13:56 -0600

    Extract WorkPromo from Work component

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,31 +1,10 @@
 import WorkCard, { WorkListItem } from "./WorkCard";
-import resume from "../assets/Krastan-resume-10-27-22.pdf";
+import WorkPromo from "./WorkPromo";
 
 const Work = () => {
   return (
     <div className="flex flex-col items-stretch">
-      <div className="bg-gray-300 p-4 mb-3 rounded text-black flex flex-col w-full items-center">
-        <h2 className="text-2xl font-bold text-center mb-2">
-          Want to be next?
-        </h2>
-        <div className="flex flex-col sm:flex-row">
-          <a
-            href={resume}
-            target="_blank"
-            rel="noreferrer"
-            className="px-3 py-2 mx-1 my-1 rounded-md border-1 shadow-md bg-gray-50 hover:bg-gray-100 text-black text-center"
-          >
-            View Resume
-          </a>
-          <a
-            href={resume}
-            download="KrastanDimitrovResume"
-            className="px-3 py-2 mx-1 my-1 rounded-md border-1 shadow-md bg-gray-50 hover:bg-gray-100 text-black text-center"
-          >
-            Download Resume
-          </a>
-        </div>
-      </div>
+      <WorkPromo />
       <div className="flex flex-col items-center">
         <WorkCard
           title="SpotOn"
