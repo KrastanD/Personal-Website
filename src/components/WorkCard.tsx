a79432fb9ba413bd698554ef4b1c22794a0b3b9d
Krastan Dimitrov <krastan.kdd@gmail.com>
2023-01-25 23:07:50 -0600

    Refactor WorkCard styling
    Make children optional
    Add width styling
    Remove children wrapper

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -2,20 +2,20 @@ type WorkCardProps = {
   title: string;
   years: string;
   position: string;
-  children: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 const WorkCard = ({ title, years, position, children }: WorkCardProps) => {
   return (
-    <div className="flex flex-col lg:flex-row bg-gray-300 my-3 rounded text-black max-w-screen-xl w-full">
-      <div className="p-4">
+    <div className="flex flex-col lg:flex-row bg-gray-300 my-3 rounded text-black w-full self-center items-center">
+      <div className="p-4 w-full">
         <div className="mb-2">
           <p className="text-center text-2xl font-bold">
             {position} @ {title}
           </p>
           <p className="text-center text-xl">{years}</p>
         </div>
-        <p className="text-l">{children}</p>
+        {children}
       </div>
     </div>
   );
