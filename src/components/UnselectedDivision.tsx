77b514730c4daa58f0c8d9d53b252a9e43366ef8
Krastan Dimitrov <krastan.kdd@gmail.com>
2024-01-03 21:53:53 -0600

    Use font lobster on unselected title division

--- src/components/UnselectedDivision.tsx
+++ src/components/UnselectedDivision.tsx
@@ -1,11 +1,12 @@
 import { DivisionName } from "../types";
 
 const UnselectedDivision = ({ division }: { division: DivisionName }) => {
-  return (
-    <p className="text-2xl md:text-xl lg:text-2xl text-center md:pt-6 text-gray-300">
-      {division}
-    </p>
-  );
+  let paragraphStyle =
+    "text-2xl md:text-xl lg:text-2xl text-center md:pt-6 text-gray-300";
+  if (division === DivisionName.Title) {
+    paragraphStyle += " font-lobster";
+  }
+  return <p className={paragraphStyle}>{division}</p>;
 };
 
 export default UnselectedDivision;
